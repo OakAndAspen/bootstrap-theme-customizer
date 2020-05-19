e8f0ad28b50f5af42efa9e1384e38e1239eb78f1
Irina Despot <irinadespot@gmail.com>
2020-05-19 09:58:51 +0200

    Copied examples

--- src/js/config/vue-router.js
+++ src/js/config/vue-router.js
@@ -1,5 +1,11 @@
-import RouteExample1 from "src/js/routes/RouteExample1.vue";
-import RouteExample2 from "src/js/routes/RouteExample2.vue";
+import ExampleAlbum from "src/js/components/examples/ExampleAlbum.vue";
+import ExampleBlog from "src/js/components/examples/ExampleBlog.vue";
+import ExampleCarousel from "src/js/components/examples/ExampleCarousel.vue";
+import ExampleCheckout from "src/js/components/examples/ExampleCheckout.vue";
+import ExampleCover from "src/js/components/examples/ExampleCover.vue";
+import ExampleDashboard from "src/js/components/examples/ExampleDashboard.vue";
+import ExamplePricing from "src/js/components/examples/ExamplePricing.vue";
+import ExampleProduct from "src/js/components/examples/ExampleProduct.vue";
 import RouteKitchenSink from "src/js/routes/RouteKitchenSink.vue";
 import Vue from "vue";
 import VueRouter from "vue-router";
@@ -13,14 +19,44 @@ const routes = [
         component: RouteKitchenSink
     },
     {
-        path: '/example1',
-        name: 'example1',
-        component: RouteExample1
+        path: '/album',
+        name: 'album',
+        component: ExampleAlbum
     },
     {
-        path: '/example2',
-        name: 'example2',
-        component: RouteExample2
+        path: '/blog',
+        name: 'blog',
+        component: ExampleBlog
+    },
+    {
+        path: '/carousel',
+        name: 'carousel',
+        component: ExampleCarousel
+    },
+    {
+        path: '/checkout',
+        name: 'checkout',
+        component: ExampleCheckout
+    },
+    {
+        path: '/cover',
+        name: 'cover',
+        component: ExampleCover
+    },
+    {
+        path: '/dashboard',
+        name: 'dashboard',
+        component: ExampleDashboard
+    },
+    {
+        path: '/pricing',
+        name: 'pricing',
+        component: ExamplePricing
+    },
+    {
+        path: '/product',
+        name: 'product',
+        component: ExampleProduct
     }
 ];
 
